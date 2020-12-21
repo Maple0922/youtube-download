10b91bbac504ffaa36caa43639a76916a6ca43a5
Futo Nakajima <maple0922wh-142857@i.softbank.jp>
2020-12-21 12:23:19 +0900

    Delete: unnnecessary files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 import './sass/style.scss';
 import app from './js/vue';
-import insertImage from './js/insertImage';
-import defaultImage from './images/default.jpg';
-
-insertImage('default-image',defaultImage);
--- src/js/insertImage.js
+++
@@ -1,6 +0,0 @@
-// insert image
-
-export default function insertImage(className,url){
-  const img = document.querySelector('.' + className);
-  img.src = url;
-}
